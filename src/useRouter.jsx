e9643008b2a88b55c89faaf4f9e879f052cef2f8
unknown <namlshe176301@fpt.edu.vn>
2024-12-05 00:09:33 +0700

    fix router

--- src/useRouter.jsx
+++ src/useRouter.jsx
@@ -9,25 +9,6 @@ import Home from "./page/Home";
 import ForgotPassword from "./page/Forgotpassword";
 import PaymentMethod from "./page/PaymentMethod";
 import RenterCar from "./page/RenterCar";
-import HistoryRentDriver from "./page/admin/HistoryRentDriver/HistoryRentDriver";
-import ListVehicleRent from "./page/admin/ListVehicleRent/ListVehicleRent";
-import LossCostType from "./page/admin/LossCostType/LossCostType";
-import TypeOfDriver from "./page/admin/TypeOfDriver/TypeOfDriver";
-import Analytics from "./page/admin/analytics/Analytics";
-import FixedCosts from "./page/admin/fixedCosts/FixedCosts";
-import DriverList from "./page/admin/managerDriver/ListDriver";
-import PromotionList from "./page/admin/managerPromotion/PromotionList";
-import ListRequest from "./page/admin/managerRequest/ListRequest";
-import RequestDetail from "./page/admin/managerRequest/RequestDetail";
-import RoleList from "./page/admin/managerRole/RoleList";
-import TripList from "./page/admin/managerTrip/TripList";
-import UserList from "./page/admin/managerUser/UserList";
-import VehicleList from "./page/admin/managerVehicle/ListVehicle";
-import DriverCreateTicket from "./page/driver/DriverCreateTicket";
-import DriverRequests from "./page/driver/DriverRequest";
-import History from "./page/driver/History";
-import DriverLogin from "./page/driver/Login"; // Import trang Driver Login
-import UpdateTicket from "./page/driver/UpdateTicket";
 import ChangePassword from "./page/profile/ChangePassword";
 import Info from "./page/profile/Info";
 import MyOrder from "./page/profile/MyOrder";
@@ -86,53 +67,8 @@ export default function useRouteElements() {
       path: "/checkout",
       element: <Checkout />,
     },
-    {
-      path: "/dashboard",
-      element: <DefaultLayout />,
-      children: [
-        { path: "analytics", element: <Analytics /> },
-        { path: "HistoryRentDriver", element: <HistoryRentDriver /> },
-        { path: "user", element: <UserList /> },
-        { path: "role", element: <RoleList /> },
-        { path: "driver", element: <DriverList /> },
-        { path: "promotion", element: <PromotionList /> },
-        { path: "request", element: <ListRequest /> },
-        { path: "request/:id", element: <RequestDetail /> },
-        { path: "vehicle", element: <VehicleList /> },
-        { path: "lossCostType", element: <LossCostType /> },
-        { path: "fixedCosts", element: <FixedCosts /> },
-        { path: "trip", element: <TripList /> },
-        { path: "typeOfDriver", element: <TypeOfDriver /> },
-        { path: "listVehicleRent", element: <ListVehicleRent /> },
-      ],
-    },
-    {
-      path: "/driver",
-
-      children: [
-        {
-          path: "history", // URL: /driver/history
-          element: <History />,
-        },
-        {
-          path: "login", // URL: /driver/profile
-          element: <DriverLogin />,
-        },
-        {
-          path: "update-ticket", // URL: /driver/update-ticket/:ticketId
-          element: <UpdateTicket />,
-        },
-        {
-          path: "create-ticket", // URL: /driver/create-ticket
-          element: <DriverCreateTicket />,
-        },
-        {
-          path: "requests", // URL: /driver/requests
-          element: <DriverRequests />,
-        },
-        // Thêm các route con khác nếu cần
-      ],
-    },
+    
+  
   ]);
   return routeElements;
 }
